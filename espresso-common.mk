7e8d587274f69253221d95f36cb728d12a5abecd
Ziyan <jaraidaniel@gmail.com>
2016-01-03 16:17:00 +0100

    espresso-common: cleanup
    Change-Id: Ifd7aa36f0e01941f918db60d11d352163e113617

--- espresso-common.mk
+++ espresso-common.mk
@@ -20,8 +20,6 @@ $(call inherit-product, hardware/ti/omap4/omap4.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso-common/overlay/aosp-common
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
@@ -125,12 +123,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
 PRODUCT_PACKAGES += \
-    librs_jni \
     com.android.future.usb.accessory
-
-# Feature live wallpaper
-PRODUCT_COPY_FILES += \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072 \
