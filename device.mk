145eaf8fe5fa026a18e006e19343aeab458995ac
Ziyan <jaraidaniel@gmail.com>
2016-04-07 19:04:34 +0200

    Add libsecril-compat
    Parts of the code taken from tuna.
    
    Change-Id: I861752d52f161b1e17c15cd8131105c08eea0585

---
+++ device.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2013 OmniROM Project
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Include espresso-common makefile
+$(call inherit-product, device/samsung/espressowifi/espresso-common.mk)
+
+# Include 3g overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso3g/overlay/aosp
+
+# Audio configs
+PRODUCT_COPY_FILES += \
+    device/samsung/espresso3g/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    device/samsung/espresso3g/audio/tiny_hw_espresso.xml:system/etc/sound/espresso \
+    device/samsung/espresso3g/audio/tiny_hw_espresso10.xml:system/etc/sound/espresso10
+
+# Packages
+PRODUCT_PACKAGES += \
+    SamsungServiceMode
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
+    ro.telephony.ril_class=SamsungOmap4RIL
+
+PRODUCT_PACKAGES += \
+    libsecril-client \
+    libsecril-compat
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Use the non-open-source parts, if they're present
+$(call inherit-product-if-exists, vendor/samsung/espresso3g/espresso3g-vendor.mk)
