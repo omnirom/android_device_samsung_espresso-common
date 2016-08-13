576ea90b2bb36284998b83bfaa7f1c88c0beb2fb
Andreas Blaesius <skate4life@gmx.de>
2016-08-13 15:46:38 +0200

    Some more cleanup
    Change-Id: Ie3e7a446084838891e2024067cc4cffce14b7c10

--- espresso-common.mk
+++ espresso-common.mk
@@ -125,7 +125,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
-PRODUCT_CHARACTERISTICS := tablet 
+PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
