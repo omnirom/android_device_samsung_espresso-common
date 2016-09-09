65ad202ae1d1e3ee0af66b6cb83ed5536e642727
Ziyan <jaraidaniel@gmail.com>
2016-09-09 13:48:59 +0200

    Remove custom libril
    Change-Id: I561608662bc207a208da81d461a2532ca0a2f5af

--- BoardConfig3g.mk
+++ BoardConfig3g.mk
@@ -1,7 +1,4 @@
 include device/samsung/espresso-common/BoardConfigCommon.mk
 
-TARGET_SPECIFIC_HEADER_PATH += device/samsung/espresso-common/include-3g
-
 # RIL
-BOARD_PROVIDES_LIBRIL := true
 BOARD_RIL_CLASS := ../../../device/samsung/espresso-common/ril
