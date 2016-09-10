a752a4c98fd3f42b7dcf2b21c758853928084d0a
Ziyan <jaraidaniel@gmail.com>
2016-09-10 09:36:38 +0200

    Revert "espresso-common: Disable ashmem tracking in parcels"
    Change-Id: I187c144172a64f50dd4e8ccf78952ae3524c1807

--- BoardConfig3g.mk
+++ BoardConfig3g.mk
@@ -5,4 +5,3 @@ TARGET_SPECIFIC_HEADER_PATH += device/samsung/espresso-common/include-3g
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 BOARD_RIL_CLASS := ../../../device/samsung/espresso-common/ril
-COMMON_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
